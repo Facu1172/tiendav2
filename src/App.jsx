d40b3489ba85dbcf7ac0721d568b232d11cf5829
Facu1172 <Facu172@hotmail.com>
2022-12-07 23:30:19 -0300

    tiendaV2

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Home from './components/Home';
+import Navbar from './components/Navbar'
+import {Routes, Route} from 'react-router-dom';
+import Products from './components/Products';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/products" element={<Products/>} />
+      </Routes>
+    </>
+  )
+}
+
+export default App;
